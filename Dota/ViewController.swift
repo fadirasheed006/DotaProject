419f3f64e348403f65936d328c2dc24fbf40e2e8
fahad rasheed <fahadrasheed@fahads-mac.local>
2020-09-08 00:40:23 -0700

    Initial Commit

---
+++ Dota/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dota
+//
+//  Created by fahad rasheed on 08/09/2020.
+//  Copyright © 2020 fahad rasheed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
